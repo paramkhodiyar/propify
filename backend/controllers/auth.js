1a7a320f4cb02b58949cae9b716662bf2b616c95
Param Khodiyar <paramkhodiyar@Params-MacBook-Pro.local>
2026-01-17 22:29:12 +0530

    COOKIE FIXING

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -72,7 +72,7 @@ const login = async (req, res) => {
         res
             .cookie('token', token, {
                 httpOnly: true,
-                secure: false, // set to true in production
+                secure: process.env.NODE_ENV === 'PRODUCTION', // set to true in production
                 maxAge: age,
             })
             .status(200)

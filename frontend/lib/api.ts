746b200ab0632601143bf629c44caf2449e44fce
Param Khodiyar <paramkhodiyar@Params-MacBook-Pro.local>
2026-02-09 11:26:15 +0530

    Added the new backend link

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_MODE === 'DEVELOPEMENT' ? 'http://localhost:4000/api' : 'https://propify-oer3.onrender.comapi',
+    baseURL: process.env.NEXT_PUBLIC_MODE === 'DEVELOPEMENT' ? 'http://localhost:4000/api' : 'https://propify-oer3.onrender.com/api',
     withCredentials: true,
 });
 
